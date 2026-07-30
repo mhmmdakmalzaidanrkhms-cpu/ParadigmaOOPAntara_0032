8f9de01f3fedd8cfd005c21fa725d015a5bef1a4
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-07-30 15:34:52 +0700

    Menambahkan implementasi kelas Barang dan metode InputData untuk penginputan data barang

--- paradigmaoop2.cpp
+++ paradigmaoop2.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 class Barang{
 public :
-    
+    string nama;
+    int jumlah;
+    string kategori;
+    string TahunProduksi;
+
+    void InputData(){
+        cout << "==============Input Data=============" << endl;
+        cout << "Masukan nama barang : ";
+        cin >> nama;
+        cout << "Masukan jumlah barang : ";
+        cin >> jumlah;
+        cout << "Masukan kategori barang : ";
+        cin >> kategori;
+        cout << "Masukan tahun produksi : ";
+        cin >> TahunProduksi;
+    }
 };
